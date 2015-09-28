ab31ea5fd023a0498074532a7d501c6fa2a619e0
unknown <n440479@Z4437-E6CE-CD74.uk.ad.ba.com>
2015-09-28 11:16:31 +0100

    Adding problems missing from git hub

--- src/euler/Problem1.java
+++ src/euler/Problem1.java
@@ -8,7 +8,7 @@ public class Problem1 {
         int total = 0;
 
         for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 | i % 5 == 0) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 total += i;
             }
 
